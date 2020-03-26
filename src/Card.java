a185e22624fecd796c396d8bcf69a65a9b63bc64
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2020-03-26 11:42:33 -0600

    CARD- building card class and test

---
+++ src/Card.java
@@ -0,0 +1,6 @@
+public class Card {
+    public Card(String s, String juneau, String geography) {
+        
+    }
+    //code for cards here
+}
---
+++ src/CardTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CardTest {
+    @Test
+    void testCardExists() {
+       final Card card1 = new Card("What is the capital of Alaska?", "Juneau", "Geography");
+
+        testCardExists();
+    }
+}
