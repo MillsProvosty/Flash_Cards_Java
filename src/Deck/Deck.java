1ea067526051b6328b796857e1248621a99dc4e2
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2020-03-31 14:40:38 -0600

    DECK- Adds cards to deck

--- src/Deck/Deck.java
+++ src/Deck/Deck.java
@@ -1,4 +1,16 @@
 package Deck;
 
+import Card.Card;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Deck {
+
+    ArrayList<Card> cards= new Arrayist<>();
+
+    public Deck(final ArrayList cards) {
+        this.cards = cards;
+    }
+
 }
--- src/Deck/DeckTest.java
+++ src/Deck/DeckTest.java
@@ -4,6 +4,8 @@ import Card.Card;
 import Turn.Turn;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -15,7 +17,8 @@ public class DeckTest {
         final Card card1 = new Card("What is the capital of Alaska?", "Juneau", "Geography");
         final Card card2 = new Card("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", "STEM");
         final Card card3 = new Card("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", "STEM");
-        
+        final Deck deck1 = new Deck(card1, card2, card3);
+
 
         //when
 
