6eae1f7e638ba31e78a67993810e55cf6d76788c
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2020-04-23 14:17:28 -0600

    Round2 current card returns first card in deck

--- src/Round/Round.java
+++ src/Round/Round.java
@@ -1,17 +1,25 @@
 package Round;
 
 
+import Card.Card;
 import Deck.Deck;
+import Turn.Turn;
 
 public class Round {
     public Deck deck;
+    public Turn turn;
+
     public Round(final Deck deck){
         this.deck = deck;
         System.out.println(deck.cards);
         System.out.println("Creating a round!");
     }
 
+    public Card currentCard(){
+      return deck.cards.iterator().next();
+    }
 
 
 
 }
+
--- src/Round/RoundTest.java
+++ src/Round/RoundTest.java
@@ -28,8 +28,25 @@ public class RoundTest {
 
         //then
         assertEquals(round1.deck.countCards(), 3);
+    }
 
+    @Test
+    void testCurrentCard(){
+        //given
+        final Card carda = new Card("What is the capital of Alaska?", "Juneau", "Geography");
+        final Card cardb = new Card("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", "STEM");
+        final Card cardc = new Card("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", "STEM");
+        final List<Card> cards2 = new ArrayList<Card>();
+        cards2.add(carda);
+        cards2.add(cardb);
+        cards2.add(cardc);
+        final Deck deck1 = new Deck(cards2);
+        final Round round1 = new Round(deck1);
 
+        //when
+        round1.currentCard();
 
+        //then
+        assertEquals(round1.currentCard(), carda);
     }
 }
